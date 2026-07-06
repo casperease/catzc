7b0038cb3002da51f0e5579f79b01e1b49e9e51e
Casper Leon Nielsen <casper.nielsen@gmail.com>
2026-07-06 18:58:22 +0200

    ...

--- automation/Catzc.Azure.Templates/tests/Get-BicepParametersFileName.Tests.ps1
+++ automation/Catzc.Azure.Templates/tests/Get-BicepParametersFileName.Tests.ps1
@@ -1,13 +1,20 @@
 Describe 'Get-BicepParametersFileName' -Tag 'L0', 'logic' {
-    It 'names the artifact by subscription and config (parameters.SUBSCRIPTION.CONFIG.json)' {
-        & (Get-Module Catzc.Azure.Templates) { Get-BicepParametersFileName -Environment dev -Subscription shared_nonprod } |
-            Should -Be 'parameters.shared_nonprod.dev.json'
-        & (Get-Module Catzc.Azure.Templates) { Get-BicepParametersFileName -Environment dev -Slot 001 -Subscription shared_nonprod } |
-            Should -Be 'parameters.shared_nonprod.dev-001.json'
+    It 'names a configuration-root artifact by config alone (parameters.CONFIG.json)' {
+        & (Get-Module Catzc.Azure.Templates) { Get-BicepParametersFileName -Environment dev } |
+            Should -Be 'parameters.dev.json'
+        & (Get-Module Catzc.Azure.Templates) { Get-BicepParametersFileName -Environment dev -Slot 001 } |
+            Should -Be 'parameters.dev-001.json'
     }
 
-    It 'keys distinct subscriptions to distinct artifact names for the same env+slot' {
-        & (Get-Module Catzc.Azure.Templates) { Get-BicepParametersFileName -Environment dev -Subscription apex_nonprod } |
-            Should -Be 'parameters.apex_nonprod.dev.json'
+    It 'names a customer artifact by customer and config (parameters.CUSTOMER.CONFIG.json)' {
+        & (Get-Module Catzc.Azure.Templates) { Get-BicepParametersFileName -Environment dev -Customer acme } |
+            Should -Be 'parameters.acme.dev.json'
+        & (Get-Module Catzc.Azure.Templates) { Get-BicepParametersFileName -Environment dev -Slot 001 -Customer acme } |
+            Should -Be 'parameters.acme.dev-001.json'
+    }
+
+    It 'keys distinct customers to distinct artifact names for the same env+slot' {
+        & (Get-Module Catzc.Azure.Templates) { Get-BicepParametersFileName -Environment dev -Customer globex } |
+            Should -Be 'parameters.globex.dev.json'
     }
 }
